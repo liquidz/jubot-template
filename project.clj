046e7178288e41135f3f963966a866f7c48dd4ec
liquidz <liquidz.uo@gmail.com>
2015-02-21 14:48:35 +0900

    update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jubot/lein-template "0.0.1-SNAPSHOT-7"
+(defproject jubot/lein-template "0.0.1-SNAPSHOT-9"
   :description "jubot template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
